a24d291ae77dc47bbce1e0a2b835d80609fd2126
EndrewSilveira <endrew.silveira21@gmail.com>
2023-06-09 01:48:09 -0300

    merge

--- app/src/main/java/br/com/example/pind/screens/estoque/EstoqueDialog.kt
+++ app/src/main/java/br/com/example/pind/screens/estoque/EstoqueDialog.kt
@@ -3,23 +3,25 @@ package br.com.example.pind.screens.estoque
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.Window
 import android.widget.Button
 import android.widget.EditText
-import android.widget.ImageView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatDialogFragment
 import br.com.example.pind.R
-import br.com.example.pind.modal.estoque.EstoqueItem
-import kotlin.random.Random
-
+import br.com.example.pind.api.caller.ProductsCaller
+import br.com.example.pind.api.models.ProductModel
+import br.com.example.pind.api.services.ProductsService
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class EstoqueDialog : AppCompatDialogFragment() {
 
-    var onAddItem: ((item: EstoqueItem) -> Unit)? = null
+    var onAddItem: (() -> Unit)? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,39 +42,38 @@ class EstoqueDialog : AppCompatDialogFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val etProduto = view.findViewById<EditText>(R.id.et_produto)
-        val etQuantidade = view.findViewById<EditText>(R.id.et_quantidade)
-        val etDataFrabricacao = view.findViewById<EditText>(R.id.et_data_fabricação)
-        val etDataValidade = view.findViewById<EditText>(R.id.et_data_validade)
-        val etUnidadeMedida = view.findViewById<EditText>(R.id.et_unidade_medida)
-        val btnAdicionarProduto = view.findViewById<Button>(R.id.btn_add_product)
-        val btnFechar = view.findViewById<ImageView>(R.id.iv_fechar)
+        val btnAddProduct = view.findViewById<Button>(R.id.btn_add_product)
+        val textProduct = view.findViewById<EditText>(R.id.et_produto)
+        val textQuantity = view.findViewById<EditText>(R.id.et_quantidade)
+        val textPrice = view.findViewById<EditText>(R.id.price)
+        val textMeasurement = view.findViewById<EditText>(R.id.unit_measurement)
 
-        btnFechar.setOnClickListener {
+        btnAddProduct.setOnClickListener {
+            val call = ProductsCaller(requireContext()).productsService().create(ProductsService :: class.java).postProduct(ProductModel(
+                textProduct.text.toString(),
+                textPrice.text.toString().toInt(),
+                textQuantity.text.toString().toInt(),
+                textMeasurement.text.toString()
+            ))
+            call.enqueue(object : Callback<ProductModel> {
+                override fun onResponse(
+                    call: Call<ProductModel>,
+                    response: Response<ProductModel>
+                ) {
+                    response.body()?.let {
+                        Log.i("Product", it.toString())
+                        onAddItem?.invoke()
+                    }
+                }
+                override fun onFailure(call: Call<ProductModel>, t: Throwable) {
+                    t?.message?.let { it1 -> Log.e("Erro", it1) }
+
+                }
+
+            }
+            )
             dismiss()
         }
 
-        btnAdicionarProduto.setOnClickListener {
-            if (etProduto.text.toString().isNotEmpty() &&
-                etQuantidade.text.toString().isNotEmpty() &&
-                etDataFrabricacao.text.toString().isNotEmpty() &&
-                etDataValidade.text.toString().isNotEmpty() &&
-                etUnidadeMedida.text.toString().isNotEmpty()
-            ) {
-                onAddItem?.invoke(
-                    EstoqueItem(
-                        Random.nextInt().toString(),
-                        etProduto.text.toString(),
-                        etQuantidade.text.toString(),
-                        etDataFrabricacao.text.toString(),
-                        etDataValidade.text.toString(),
-                        etUnidadeMedida.text.toString()
-                    )
-                )
-                dismiss()
-            } else {
-                Toast.makeText(requireContext(), "É necessário preencher os campos.", Toast.LENGTH_SHORT).show()
-            }
-        }
     }
 }
--- app/src/main/java/br/com/example/pind/screens/estoque/EstoqueFragment.kt
+++ app/src/main/java/br/com/example/pind/screens/estoque/EstoqueFragment.kt
@@ -6,13 +6,19 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
-import br.com.example.pind.screens.estoque.adapter.EstoqueListAdapter
 import br.com.example.pind.R
-import br.com.example.pind.modal.estoque.EstoqueItem
+import br.com.example.pind.api.caller.ProductsCaller
+import br.com.example.pind.api.models.ProductModel
+import br.com.example.pind.api.services.ProductsService
+import br.com.example.pind.screens.estoque.adapter.EstoqueListAdapter
 import com.google.android.material.floatingactionbutton.FloatingActionButton
-import kotlin.random.Random
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class EstoqueFragment : Fragment() {
+
+    private var estoqueAdapter: EstoqueListAdapter? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -24,47 +30,47 @@ class EstoqueFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         val recyclerView = view.findViewById<RecyclerView>(R.id.rv_estoque)
-        val btnAdd =  view.findViewById<FloatingActionButton>(R.id.add_btn)
-        val btnRemoveEstoque =  view.findViewById<FloatingActionButton>(R.id.remove_btn)
+        val btnAdd = view.findViewById<FloatingActionButton>(R.id.add_btn)
+        val btnRemoveEstoque = view.findViewById<FloatingActionButton>(R.id.remove_btn)
         val estoqueDialog = EstoqueDialog()
 
-        // lista mockada
-        val estoqueList = mutableListOf(
-            EstoqueItem(
-                Random.nextInt().toString(),
-                "Exemplo",
-                "0",
-                "05/06/2023",
-                "08/06/2023",
-                "Kg"
-            )
-        )
-        val estoqueAdapter = EstoqueListAdapter(estoqueList)
-
-        recyclerView.adapter = estoqueAdapter
+        getProduct(recyclerView)
 
         btnAdd.setOnClickListener {
-            estoqueDialog.show(requireActivity().supportFragmentManager, "dialogAdd")
+            estoqueDialog.show(parentFragmentManager, "dialogAdd")
         }
 
         estoqueDialog.onAddItem = {
-            estoqueList.add(it)
-            estoqueAdapter.notifyItemInserted(estoqueList.indexOf(it))
+            getProduct(recyclerView)
         }
 
         btnRemoveEstoque.setOnClickListener {
-            estoqueAdapter.onRemoveEnable()
-            estoqueAdapter.notifyItemRangeChanged(
-                estoqueList.indexOfFirst { true },
-                estoqueList.size
-            )
+
         }
 
-        estoqueAdapter.onItemRemoved = {
-            val index = estoqueList.indexOf(it)
-            estoqueList.remove(it)
-            estoqueAdapter.notifyItemRemoved(index)
-        }
+    }
+
+    private fun getProduct(recyclerView: RecyclerView) {
+        val call = ProductsCaller(requireContext())
+            .productsService()
+            .create(ProductsService::class.java)
+            .getProduct()
+
+        call.enqueue(object : Callback<List<ProductModel>> {
+            override fun onResponse(
+                call: Call<List<ProductModel>>,
+                response: Response<List<ProductModel>>
+            ) {
+                if (response.isSuccessful) {
+                    estoqueAdapter = response.body()?.let { EstoqueListAdapter(it) }
+                    recyclerView.adapter = estoqueAdapter
+                }
+            }
+
+            override fun onFailure(call: Call<List<ProductModel>>, t: Throwable) {}
+
+        })
     }
 }
